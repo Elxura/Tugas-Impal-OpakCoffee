3320387d578c2f3128f0ed875676c43bc4220d0f
M MASYUD ALAM <136221616+Elxura@users.noreply.github.com>
2024-12-17 17:56:42 +0700

    Update koneksi.php

--- OpakCoffee-User/login-page/koneksi.php
+++ OpakCoffee-User/login-page/koneksi.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root";        
 $password = "";           
-$database = "tubes_kopi"; 
+$database = "opak_kopi"; 
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
